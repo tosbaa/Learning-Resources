d2c92836e7dc9ae4219e2222b576129d940338d6
Yagiz Kocer <yagizkocer@gmail.com>
2021-02-13 00:26:16 +0300

    Added template and style to AddResource component

--- src/components/learning-resource/AddResource.vue
+++ src/components/learning-resource/AddResource.vue
@@ -1,3 +1,55 @@
 <template>
-  <h2>Add Resource</h2>
+  <base-card>
+    <h2>Add Resource</h2>
+    <form>
+      <div class="form-control">
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title" />
+      </div>
+      <div class="form-control">
+        <label for="description">Description</label>
+        <textarea
+          name="description"
+          id="description"
+          cols="30"
+          rows="10"
+        ></textarea>
+      </div>
+      <div class="form-control">
+        <label for="link">Link</label>
+        <input type="url" name="link" id="link" />
+      </div>
+      <base-button type="submit">Add Resource</base-button>
+    </form>
+  </base-card>
 </template>
+
+
+
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+</style>
