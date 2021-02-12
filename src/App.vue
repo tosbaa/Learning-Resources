db929c938ab578df1fdf70697b499a7c9d365f76
Yagiz Kocer <yagizkocer@gmail.com>
2021-02-12 16:21:03 +0300

    Created, Binded Learning Resource Comp

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <ul>
-
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
   </ul>
 </template>
 
 <script>
+import LearningResource from "./components/learning-resource/LearningResource.vue";
 export default {
+  components: {
+    LearningResource,
+  },
   data() {
     return {
       storedResources: [
         {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The official Vue.js documentation.',
-          link: 'https://vuejs.org'
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The official Vue.js documentation.",
+          link: "https://vuejs.org",
         },
         {
-          id: 'google',
-          title: 'Google',
-          description: 'Most popular search engine',
-          link: 'https://www.google.com'
+          id: "google",
+          title: "Google",
+          description: "Most popular search engine",
+          link: "https://www.google.com",
         },
-      ]
-    }
-  }
-}
+      ],
+    };
+  },
+};
 </script>
---
+++ src/components/learning-resource/LearningResource.vue
@@ -0,0 +1,36 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ title }}</h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p>{{ Description }}</p>
+    <nav>
+      <a :href="link">View Resource</a>
+    </nav>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: "Learning Resource Title",
+    },
+    description: {
+      type: String,
+      required: true,
+      default: "Learning Resource Description",
+    },
+    link: {
+      type: String,
+      required: true,
+      defualt: "about:blank",
+    },
+  },
+};
+</script>
