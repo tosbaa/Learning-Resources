7bceac160a3b3de8f1e53ef1d2fd2b07cbd35d41
Yagiz Kocer <yagizkocer@gmail.com>
2021-02-12 20:45:20 +0300

    TheResource component created

---
+++ src/components/learning-resource/TheResources.vue
@@ -0,0 +1,30 @@
+<template>
+  <base-card>
+    <base-button @click="selecTab('stored-resources')"
+      >Stored Resources</base-button
+    >
+    <base-button @click="selecTab('add-resources')">Add Resource</base-button>
+    <component :is="selectedTab"></component>
+  </base-card>
+</template>
+
+<script>
+import StoredResources from "./StoredResources.vue"
+import AddResource from "./AddResource.vue"
+export default {
+  components: {
+    StoredResources, AddResource
+  },
+  
+  data() {
+    return {
+      selectedTab: "stored-resources",
+    };
+  },
+  methods: {
+    selectTab(tab) {
+      this.selectedTab = tab;
+    },
+  },
+};
+</script>
