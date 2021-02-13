aa394530df320f4ee78353cfe73174318867e6d2
Yagiz Kocer <yagizkocer@gmail.com>
2021-02-13 14:07:07 +0300

    BaseDialog UI component created, added globally

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import BaseCard from "./UI/BaseCard.vue";
 import BaseButton from "./UI/BaseButton.vue"
+import BaseDialog from "./UI/BaseDialog.vue"
 
 var app = createApp(App);
 app.component('base-card', BaseCard);
 app.component('base-button', BaseButton);
+app.component('base-dialog', BaseDialog);
 app.mount("#app");
