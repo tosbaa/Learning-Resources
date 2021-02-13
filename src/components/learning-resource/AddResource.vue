cf488f4f95f33a77f1ff2b3702c3af7c02b58ac8
Yagiz Kocer <yagizkocer@gmail.com>
2021-02-13 14:07:55 +0300

    Dialog  added for invalid inputs

--- src/components/learning-resource/AddResource.vue
+++ src/components/learning-resource/AddResource.vue
@@ -1,10 +1,19 @@
 <template>
+  <base-dialog v-if="inputIsInvalid" @close="closeDialog" title="Invalid Input">
+    <template #default>
+      <p>Unfortunately, at least one input is invalid</p>
+      <p>Make sure you entered at least few characters for each input</p>
+    </template>
+    <template #actions>
+      <base-button @click="closeDialog"> Okay </base-button>
+    </template>
+  </base-dialog>
   <base-card>
     <h2>Add Resource</h2>
     <form @submit.prevent="submitForm">
       <div class="form-control">
         <label for="title">Title</label>
-        <input type="text" name="title" id="title" ref="titleInput"/>
+        <input type="text" name="title" id="title" ref="titleInput" />
       </div>
       <div class="form-control">
         <label for="description">Description</label>
@@ -18,7 +27,7 @@
       </div>
       <div class="form-control">
         <label for="link">Link</label>
-        <input type="url" name="link" id="link" ref="linkInput"/>
+        <input type="url" name="link" id="link" ref="linkInput" />
       </div>
       <base-button type="submit">Add Resource</base-button>
     </form>
@@ -26,17 +35,31 @@
 </template>
 
 <script>
+import BaseDialog from "../../UI/BaseDialog.vue";
 export default {
-  inject: ['addResource'],
+  components: { BaseDialog },
+  inject: ["addResource"],
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
   methods: {
     submitForm() {
-      const titleInput = this.$refs.titleInput.value;
-      const descInput = this.$refs.descInput.value;
-      const urlInput = this.$refs.linkInput.value;
-      this.addResource(titleInput, descInput, urlInput);
+      const titleInput = this.$refs.titleInput.value.trim();
+      const descInput = this.$refs.descInput.value.trim();
+      const urlInput = this.$refs.linkInput.value.trim();
+      if (titleInput === "" || descInput === "" || urlInput === "") {
+        this.inputIsInvalid = true;
+      } else {
+        this.addResource(titleInput, descInput, urlInput);
+      }
     },
-  }
-}
+    closeDialog() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
 </script>
 
 
